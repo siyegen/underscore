b442e6a7d22ce6f6d0216fc6d015b6c828e3b22c
David Tomberlin <david.tomberlin@sendgrid.com>
2012-01-12 23:40:22 -0800

    added test for changes to isEmpty

--- test/objects.js
+++ test/objects.js
@@ -365,6 +365,7 @@ $(document).ready(function() {
     ok(_.isEmpty(), 'undefined is empty');
     ok(_.isEmpty(''), 'the empty string is empty');
     ok(!_.isEmpty('moe'), 'but other strings are not');
+    ok(!_.isEmpty(1), '1 is not empty');
 
     var obj = {one : 1};
     delete obj.one;
